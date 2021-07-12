f41c097608778128eafce38b6942772997b2c893
Jorge Adolfo Ramírez Uresti <juresti@tec.mx>
2021-07-12 14:25:02 -0500

    Quito soluciones
    Quito las soluciones de todos los ejercicios para que al alumno ya no le aparezcan.
    Sin embargo, en el commit anterior estaban todas la soluciones.

--- Ejercicio Memo/prueba.py
+++ Ejercicio Memo/prueba.py
@@ -1,11 +1,5 @@
 def impares(lista):
 
     #Write your code here
-
-    #Here is the solution
-    impar = 0
-    for num  in lista:
-        if num%2 != 0:
-            impar += 1
-    return impar
+    pass
     
